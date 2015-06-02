d8fc0ec9925cc29b47418a8732825adcad780515
Fast-Dev-Kit <2642754767@qq.com>
2015-06-02 15:02:50 +0800

    bug fix

--- src/net/datafans/android/common/widget/table/refresh/adapter/BGAListViewAdapter.java
+++ src/net/datafans/android/common/widget/table/refresh/adapter/BGAListViewAdapter.java
@@ -4,7 +4,6 @@ import net.datafans.android.common.R;
 import net.datafans.android.common.widget.table.refresh.ListViewAdapter;
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;

9ddad4576a8c03108b856e385ac2818073590171
Fast-Dev-Kit <2642754767@qq.com>
2015-06-09 20:02:50 +0800

    增加 root controller 设置 no_margin

--- android-common-lib/src/main/java/net/datafans/android/common/widget/controller/Controller.java
+++ android-common-lib/src/main/java/net/datafans/android/common/widget/controller/Controller.java
@@ -4,6 +4,7 @@ import net.datafans.android.common.R;
 import net.datafans.android.common.config.AndroidCommon;
 import net.datafans.android.common.data.service.BaseResponse;
 import net.datafans.android.common.lib.systembar.SystemBarTintManager;
+
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.ActionBar;
@@ -22,97 +23,104 @@ import android.widget.Toast;
 
 public abstract class Controller extends Activity {
 
-	protected ActionBar actionBar;
+    protected ActionBar actionBar;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-		// requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_base);
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        super.onCreate(savedInstanceState);
+        if (enableMargin())
+            setContentView(R.layout.activity_base);
+        else
+            setContentView(R.layout.activity_base_no_margin);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-		// actionBar = getActionBar();
-		// actionBar.setHomeButtonEnabled(true);
-		// actionBar.setDisplayHomeAsUpEnabled(true);
+        // actionBar = getActionBar();
+        // actionBar.setHomeButtonEnabled(true);
+        // actionBar.setDisplayHomeAsUpEnabled(true);
 
-		initActionBar();
-		initFragment();
-	}
+        initActionBar();
+        initFragment();
+    }
 
-	@TargetApi(Build.VERSION_CODES.KITKAT)
-	@SuppressLint("InlinedApi")
-	private void initActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-			Window window = getWindow();
-			// Translucent status bar
-			window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-					WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-			// Translucent navigation bar
-			window.setFlags(
-					WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
-					WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-		}
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @SuppressLint("InlinedApi")
+    private void initActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // Translucent navigation bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
 
-		// 创建状态栏的管理实例
-		SystemBarTintManager tintManager = new SystemBarTintManager(this);
-		// 激活状态栏设置
-		tintManager.setStatusBarTintEnabled(true);
-		// 激活导航栏设置
-		tintManager.setNavigationBarTintEnabled(true);
+        // 创建状态栏的管理实例
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        // 激活状态栏设置
+        tintManager.setStatusBarTintEnabled(true);
+        // 激活导航栏设置
+        tintManager.setNavigationBarTintEnabled(true);
 
-		tintManager.setTintColor(getStatusBarColor());
+        tintManager.setTintColor(getStatusBarColor());
 
-		// tintManager.setNavigationBarTintColor(Color.RED);
+        // tintManager.setNavigationBarTintColor(Color.RED);
 
-		// 设置一个样式背景给导航栏
-		// tintManager.setNavigationBarTintResource(R.drawable.back_comment_tab);
-		// 设置一个状态栏资源
-		// tintManager.setStatusBarTintDrawable(MyDrawable);
-	}
+        // 设置一个样式背景给导航栏
+        // tintManager.setNavigationBarTintResource(R.drawable.back_comment_tab);
+        // 设置一个状态栏资源
+        // tintManager.setStatusBarTintDrawable(MyDrawable);
+    }
 
-	private void initFragment() {
+    private void initFragment() {
 
-		FragmentManager fm = getFragmentManager();
-		FragmentTransaction transaction = fm.beginTransaction();
-		transaction.replace(R.id.container, getRootFragment());
-		transaction.commit();
-	}
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.container, getRootFragment());
+        transaction.commit();
+    }
 
-	protected abstract Fragment getRootFragment();
+    protected abstract Fragment getRootFragment();
 
-	protected int getStatusBarColor() {
-		int color = AndroidCommon.getAppearence().getStatusBarTintColor();
-		if (color != 0) {
-			return color;
-		}
-		return Color.BLACK;
-	}
+    protected boolean enableMargin() {
+        return true;
+    }
 
-	protected void onStatusOk(BaseResponse response, Class<?> type) {
+    protected int getStatusBarColor() {
+        int color = AndroidCommon.getAppearence().getStatusBarTintColor();
+        if (color != 0) {
+            return color;
+        }
+        return Color.BLACK;
+    }
 
-	}
+    protected void onStatusOk(BaseResponse response, Class<?> type) {
 
-	protected void onStatusError(BaseResponse response) {
-		Log.e("statusError", response.toString());
-		Toast toast = Toast.makeText(this, response.getErrorMsg(),
-				Toast.LENGTH_SHORT);
-		toast.show();
-	}
+    }
 
-	protected void onRequestError(int errorCode, byte[] errorResponse,
-			Throwable throwable) {
-		if (errorCode == -2) {
-			Log.e("exception", "network exception");
-			Toast toast = Toast.makeText(this, "网络异常", Toast.LENGTH_SHORT);
-			toast.show();
-		}
+    protected void onStatusError(BaseResponse response) {
+        Log.e("statusError", response.toString());
+        Toast toast = Toast.makeText(this, response.getErrorMsg(),
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
-		if (errorCode == -1) {
-			Log.e("exception", "data_parse_exception");
-			Toast toast = Toast.makeText(this, "数据解析错误", Toast.LENGTH_SHORT);
-			toast.show();
-		}
-	}
+    protected void onRequestError(int errorCode, byte[] errorResponse,
+                                  Throwable throwable) {
+        if (errorCode == -2) {
+            Log.e("exception", "network exception");
+            Toast toast = Toast.makeText(this, "网络异常", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        if (errorCode == -1) {
+            Log.e("exception", "data_parse_exception");
+            Toast toast = Toast.makeText(this, "数据解析错误", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
 }
